2188a897af0ade440f0436e3afedff7bae30cf44
wbboydlll <wbboydlll@gmail.com>
2025-11-22 19:44:00 -0500

    added the change colors function

--- squares.js
+++ squares.js
@@ -1,12 +1,17 @@
 window.onload=function(){
     var add= document.getElementById("add");
     add.onclick = addSquare;
+
+    var colors= document.getElementById("colors")
+    add.onclick = changeColors;
+
     var squareCount = parseInt(Math.random()*21)+30;
     for(let i = 0; i < squareCount; i++) {
         addSquare();
 
     }
 }
+
 function getRandomColor(){
     var possibilites = "0123456789ABCDEF";
     var result="#";
@@ -17,9 +22,20 @@ function getRandomColor(){
     }
     return result;    
 }
+function changeColors(){
+    var squares= document.getElementById("square");
+    for (var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = getRandomColor();
+    }
+}
 function addSquare() {
     var squarearea = document.getElementById("squarearea");
     var newSquare = document.createElement("div");
+    
+    var size = parseInt(Math.random()*11)+45;
+    newSquare.style.width = size+"px";
+    newSquare.style.height = size+"px";
+    
     newSquare.className="square";
     newSquare.style.left = parseInt(Math.random()*651)+"px";
     newSquare.style.top = parseInt(Math.random()*251)+"px";
